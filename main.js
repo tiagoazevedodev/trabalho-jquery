3a9f27133a44a0b34f4ede1860ee7b7ff133b60a
tw7 <tiagogadenz@icloud.com>
2023-11-22 17:44:16 -0300

    Adicionando funcionalidade de adicionar no carrinho. Salvando as informações em objetos, e criando uma variavel Total

--- main.js
+++ main.js
@@ -1,7 +1,19 @@
+total = 0;
+const produtosCarrinho = []
 $(document).ready(function() {
-    $(".add-button").click(function() {
-        alert("Add button clicked")
+    $(".add-button").click(function(event) {
+        preco = event.target.previousElementSibling.innerHTML; /* isso é o preço */
+        preco = parseFloat(preco.slice(1, preco.length));
+        nomeProduto = event.target.previousElementSibling.previousElementSibling.innerHTML; /* isso é o Nome*/
+        total += (preco); /* isso é o total dos produtos adicionados */
         
+        produtoAtual = {      /* isso é o objeto com as informações que vai ser adicionado no array */
+            nome: nomeProduto,
+            preco: preco
+        }
+        produtosCarrinho.push(produtoAtual);
+        console.log(produtosCarrinho);
+        $(".span-quantity").html(`${total.toFixed(2)} ${nomeProduto}`);
     })
 
 });
