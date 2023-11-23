2b6f75cdbdf151fc2f72769d64e1a9c7e3d29c2f
tw7 <tiagogadenz@icloud.com>
2023-11-22 23:56:25 -0300

    Loja com nome e tema.

--- main.js
+++ main.js
@@ -3,17 +3,18 @@ const produtosCarrinho = []
 $(document).ready(function() {
     $(".add-button").click(function(event) {
         preco = event.target.previousElementSibling.innerHTML; /* isso é o preço */
-        preco = parseFloat(preco.slice(1, preco.length));
+        preco = parseFloat(preco.slice(2, preco.length));
         nomeProduto = event.target.previousElementSibling.previousElementSibling.innerHTML; /* isso é o Nome*/
+        imagemProduto =event.target.previousElementSibling.previousElementSibling.previousElementSibling.src; /* isso é a imagem*/
         total += preco; /* isso é o total dos produtos adicionados */
         
         produtoAtual = {      /* isso é o objeto com as informações que vai ser adicionado no array */
             nome: nomeProduto,
-            preco: preco
+            preco: preco,
+            imagem: imagemProduto
         }
         produtosCarrinho.push(produtoAtual);
         console.log(produtosCarrinho);
-        $(".span-quantity").html(`${total.toFixed(2)} ${nomeProduto}`);
+        $(".span-quantity").html(`R$${total.toFixed(2)} - ${produtosCarrinho.length} Produtos`);
     })
-
 });
