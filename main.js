62b1b5fe35a53a02d9b03a77150347f4f4934363
henriqueeBG <henriquebg.bg@outlook.com>
2023-12-11 16:50:51 -0300

    pagamento sem css

--- main.js
+++ main.js
@@ -122,6 +122,7 @@ function alertaCustomizado(html) {
 }
 
 $(document).ready(function() {
+    $(".receptor-finalizarCompra").hide();
     $(".cart").hide();
     $(".add-button").click(function(event) {
         preco = event.target.previousElementSibling.innerHTML; /* isso é o preço */
@@ -161,6 +162,7 @@ $(document).ready(function() {
         `)
     })
     $(".finalizar-compra").click(function(event) {
+        $(".receptor-finalizarCompra").show();
         $(".receptor-finalizarCompra").html(`
         <div class="finalizarCompra">
             <!-- Escolha de pagamento aqui -->
@@ -172,11 +174,81 @@ $(document).ready(function() {
         </div>`)
         $(".cart").hide();
         
-        
         // Pix
         $(".botao-seletor-pix").click(function(event) {
             $(".receptor-finalizarCompra").html(`
+            <div class="pagamento-pix">
+                <span class="titulo-pagamento">Pagamento via Pix</span>
+                <div class="inputs_pix">
+                    <div><span>Nome: </span><input type="text" class="input-pix"></div>
+                    <div><span>CPF: </span><input type="text" class="input-pix" maxlength="14" minlength="11"></div>
+                </div>
+                <div class="botoes-pagamento">
+                    <button class="botao-pagamento-codigo">Copiar código</button>
+                    <button class="botao-pagamento-qrcode">Gerar QR Code</button>
+                </div>
+            </div>
             `)
-        })
+            $(".botao-pagamento-codigo").click(function(event) {
+                $(".pagamento-pix").html(`
+                <div>
+                    CODIGO
+                </div>
+                <div class="botoes-pagamento">
+                    <button class="botao-pagamento">Finalizar Pagamento</button>
+                </div>
+                `)
+            }
+            )
+            $(".botao-pagamento-qrcode").click(function(event) {
+                $(".pagamento-pix").html(`
+                <div>
+                    QR CODE
+                </div>
+                <div class="botoes-pagamento">
+                    <button class="botao-pagamento">Finalizar Pagamento</button>
+                </div>
+                `)
+            }
+            )
+        }
+        )
+
+        $(".botao-seletor-credito").click(function(event) {
+
+            parc_1x = total.toFixed(2);
+            parc_2x = (total/2).toFixed(2);
+            parc_3x = ((total*1.03)/3).toFixed(2);
+            parc_4x = ((total*1.04)/4).toFixed(2);
+            parc_5x = ((total*1.05)/5).toFixed(2);
+
+            $(".receptor-finalizarCompra").html(`
+            <div class="pagamento-cartao">
+                <span class="titulo-pagamento">Pagamento via Cartão de Crédito</span>
+                <div class="inputs_cartao">
+                    <div><span>Nome: </span><input type="text" class="input-pix"></div>
+                    <div><span>Número do Cartão: </span><input type="text" class="input-pix" maxlength="20" minlength="16"></div>
+                    <div><span>Validade: </span><input type="text" class="input-pix" maxlength="5" minlength="5"></div>
+                    <div><span>CVV: </span><input type="text" class="input-pix" maxlength="3" minlength="3"></div>
+                    <div>
+                        <span>Parcelamento: 
+                            <select>
+                                <option>1x sem juros - R$ ${parc_1x}</option>
+                                <option>2x sem juros - R$ ${parc_2x}</option>
+                                <option>3x com juros - R$ ${parc_3x}</option>
+                                <option>4x com juros - R$ ${parc_4x}</option>
+                                <option>5x com juros - R$ ${parc_5x}</option>
+                            </select>
+                        <span></div>
+                </div>
+                <div class="botoes-pagamento">
+                    <button class="botao-pagamento">Finalizar Pagamento</button>
+                </div>
+            </div>
+            `)
+        }
+        )
+
+
     })
-});
+})
